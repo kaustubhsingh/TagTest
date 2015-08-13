bfdb7324be76b4153b51809ee635bd42bcd7daca
Kaustubh <kaustubh.singh@gmail.com>
2015-08-12 22:21:15 -0300

    update

--- tagtest.js
+++ tagtest.js
@@ -18,6 +18,11 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+app.get('/processinput', function(req, res) {
+
+	res.send(req.query['landingpages'] + req.query['keywords']);
+	
+});
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
