99447460aa6b063da5c07c66527f74b48c863f3d
Kaustubh <kaustubh.singh@gmail.com>
2015-08-19 02:46:48 -0300

    update

--- routes/report.js
+++ routes/report.js
@@ -22,42 +22,34 @@ function generate_report(landing_pages, keywords, res)
         request(landing_pages[count],
                 function (error, response, body) {
                     //Check for error
-                    if(error){
-                        res.render('pages/report', { landingpages: error, keywords: error} );
+                    if(error || (response.statusCode !== 200)){
+                        // res.render('pages/report', { landingpages: error, keywords: error} );
                         console.log('Error:', error);
-                        report[count] = "Couldn't download the page. Try manual inspection.";
-                        count++;
-                        continue;
+                        report[count] = "Couldn't download the page.";
+                    }
+                    else{                    
+                        
+                        if (body.indexOf(keywords) > -1) {
+                            report[count] = "Yes";
+                        }
+                        else
+                        {
+                            report[count] = "No";
+                        }
                     }
                 
-                    //Check for right status code
-                    if(response.statusCode !== 200){
-                        return console.log('Invalid Status Code Returned:', response.statusCode);
-                    }
-                
-                    //console.log(body);
-                    
-                    
-                    if (body.indexOf(keywords) > -1) {
-                        report[count] = "Yes";
-                    }
-                    else
-                    {
-                        report[count] = "No";
-                    }
-                    
                     count++;
                     console.log(count);
                     if (count < landing_pages.length){
                         console.log("case a")
-                        download(count); 
+                        download(count, 1); 
                     }
                     else
                     {
                         console.log("case b");
                         res.render('pages/report', { landingpages: landing_pages, report: report} );   
                     }
-          
+      
                     
                   });
         
